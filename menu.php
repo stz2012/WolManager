43ac6f398ee51301cba2563652d8170c9ea71933
stz2012 <tslroom@hotmail.com>
2015-11-13 10:10:18 +0900

    デバイス手動登録機能の追加

--- menu.php
+++ menu.php
@@ -10,54 +10,78 @@
 		// デバイス一覧取得
 		$DEV_LIST = UtilSQLite::getDeviceList();
 
-		if (isset($GET_DATA['mode']) && isset($GET_DATA['mac_addr']) && $GET_DATA['mac_addr'] != '')
+		if (isset($GET_DATA['mode']))
 		{
 			switch ($GET_DATA['mode'])
 			{
 				// 起動確認
 				case 'wake_confirm':
-					if (isset($GET_DATA['ip_addr']) && $GET_DATA['ip_addr'] != '')
-						$device_name = $GET_DATA['ip_addr'];
+					if (isset($GET_DATA['mac_addr']) && $GET_DATA['mac_addr'] != '')
+					{
+						if (isset($GET_DATA['ip_addr']) && $GET_DATA['ip_addr'] != '')
+							$device_name = $GET_DATA['ip_addr'];
+						else
+							$device_name = $DEV_LIST[$GET_DATA['mac_addr']];
+						$SMARTY->assign('confirm_msg', "「{$device_name}」を起動しますか？");
+						$param = array();
+						$param['mode'] = 'wake';
+						$param['mac_addr'] = $GET_DATA['mac_addr'];
+						if (isset($GET_DATA['ip_addr']) && $GET_DATA['ip_addr'] != '')
+							$param['ip_addr'] = $GET_DATA['ip_addr'];
+						$SMARTY->assign('confirm_param', UtilString::buildQueryString($param));
+					}
 					else
-						$device_name = $DEV_LIST[$GET_DATA['mac_addr']];
-					$SMARTY->assign('confirm_msg', "「{$device_name}」を起動しますか？");
-					$param = array();
-					$param['mode'] = 'wake';
-					$param['mac_addr'] = $GET_DATA['mac_addr'];
-					if (isset($GET_DATA['ip_addr']) && $GET_DATA['ip_addr'] != '')
-						$param['ip_addr'] = $GET_DATA['ip_addr'];
-					$SMARTY->assign('confirm_param', UtilString::buildQueryString($param));
+						$SMARTY->assign('inform_msg', 'パラメータが不正です。');
 					break;
 
 				// デバイス起動
 				case 'wake':
-					if (isset($GET_DATA['ip_addr']) && $GET_DATA['ip_addr'] != '')
-						$device_name = $GET_DATA['ip_addr'];
+					if (isset($GET_DATA['mac_addr']) && $GET_DATA['mac_addr'] != '')
+					{
+						if (isset($GET_DATA['ip_addr']) && $GET_DATA['ip_addr'] != '')
+							$device_name = $GET_DATA['ip_addr'];
+						else
+							$device_name = $DEV_LIST[$GET_DATA['mac_addr']];
+						UtilLog::writeLog('WOLパケットを送信 Name:'.$device_name.' MAC:'.$GET_DATA['mac_addr'], 'ACCESS');
+						$ipObj = new UtilIPv4(IP_INFO);
+						if (!WakeOnLan($ipObj->broadcast(), $GET_DATA['mac_addr']))
+							$SMARTY->assign('inform_msg', "「{$device_name}」の起動に失敗しました。");
+						else
+							$SMARTY->assign('inform_msg', "「{$device_name}」の起動に成功しました。");
+					}
 					else
-						$device_name = $DEV_LIST[$GET_DATA['mac_addr']];
-					UtilLog::writeLog('WOLパケットを送信 Name:'.$device_name.' MAC:'.$GET_DATA['mac_addr'], 'ACCESS');
-					$ipObj = new UtilIPv4(IP_INFO);
-					if (!WakeOnLan($ipObj->broadcast(), $GET_DATA['mac_addr']))
-						$SMARTY->assign('inform_msg', "「{$device_name}」の起動に失敗しました。");
-					else
-						$SMARTY->assign('inform_msg', "「{$device_name}」の起動に成功しました。");
+						$SMARTY->assign('inform_msg', 'パラメータが不正です。');
 					break;
 
-				// 追加フォーム
-				case 'append_form':
-					$SMARTY->assign('input_msg', 'デバイス名を入力してください。');
+				// 追加フォーム（手動）
+				case 'append_form1':
+					$SMARTY->assign('input_msg', 'MACアドレスとデバイス名を入力してください。');
 					$param = array();
 					$param['mode'] = 'append';
-					$param['mac_addr'] = $GET_DATA['mac_addr'];
 					$SMARTY->assign('form_param', UtilString::buildQueryString($param));
+					break;
+
+				// 追加フォーム（一覧から）
+				case 'append_form2':
+					if (isset($GET_DATA['mac_addr']) && $GET_DATA['mac_addr'] != '')
+					{
+						$SMARTY->assign('input_msg', 'デバイス名を入力してください。');
+						$param = array();
+						$param['mode'] = 'append';
+						$SMARTY->assign('form_param', UtilString::buildQueryString($param));
+						$SMARTY->assign('mac_addr', $GET_DATA['mac_addr']);
+					}
+					else
+						$SMARTY->assign('inform_msg', 'パラメータが不正です。');
 					break;
 
 				// デバイス追加
 				case 'append':
-					if (isset($POST_DATA['device_name']) && $POST_DATA['device_name'] != '')
+					if (isset($POST_DATA['mac_addr']) && $POST_DATA['mac_addr'] != '' &&
+						isset($POST_DATA['device_name']) && $POST_DATA['device_name'] != '')
 					{
 						$device_name = $POST_DATA['device_name'];
-						if (!UtilSQLite::addDeviceInfo($GET_DATA['mac_addr'], $device_name))
+						if (!UtilSQLite::addDeviceInfo($POST_DATA['mac_addr'], $device_name))
 							$SMARTY->assign('inform_msg', "「{$device_name}」の登録に失敗しました。");
 						else
 							$SMARTY->assign('inform_msg', "「{$device_name}」の登録に成功しました。");
@@ -68,29 +92,31 @@
 
 				// 削除確認
 				case 'delete_confirm':
-					$device_name = $DEV_LIST[$GET_DATA['mac_addr']];
-					$SMARTY->assign('confirm_msg', "「{$device_name}」を削除しますか？");
-					$param = array();
-					$param['mode'] = 'delete';
-					$param['mac_addr'] = $GET_DATA['mac_addr'];
-					$SMARTY->assign('confirm_param', UtilString::buildQueryString($param));
+					if (isset($GET_DATA['mac_addr']) && $GET_DATA['mac_addr'] != '')
+					{
+						$device_name = $DEV_LIST[$GET_DATA['mac_addr']];
+						$SMARTY->assign('confirm_msg', "「{$device_name}」を削除しますか？");
+						$param = array();
+						$param['mode'] = 'delete';
+						$param['mac_addr'] = $GET_DATA['mac_addr'];
+						$SMARTY->assign('confirm_param', UtilString::buildQueryString($param));
+					}
+					else
+						$SMARTY->assign('inform_msg', 'パラメータが不正です。');
 					break;
 
 				// デバイス削除
 				case 'delete':
-					$device_name = $DEV_LIST[$GET_DATA['mac_addr']];
-					if (!UtilSQLite::delDeviceInfo($GET_DATA['mac_addr']))
-						$SMARTY->assign('inform_msg', "「{$device_name}」の削除に失敗しました。");
+					if (isset($GET_DATA['mac_addr']) && $GET_DATA['mac_addr'] != '')
+					{
+						$device_name = $DEV_LIST[$GET_DATA['mac_addr']];
+						if (!UtilSQLite::delDeviceInfo($GET_DATA['mac_addr']))
+							$SMARTY->assign('inform_msg', "「{$device_name}」の削除に失敗しました。");
+						else
+							$SMARTY->assign('inform_msg', "「{$device_name}」の削除に成功しました。");
+					}
 					else
-						$SMARTY->assign('inform_msg', "「{$device_name}」の削除に成功しました。");
-					break;
-
-				// ベンダー情報更新
-				case 'update_vendor':
-					if (!UtilSQLite::updateVendorInfo(true))
-						$SMARTY->assign('inform_msg', 'ベンダー情報の更新に失敗しました。');
-					else
-						$SMARTY->assign('inform_msg', 'ベンダー情報の更新に成功しました。');
+						$SMARTY->assign('inform_msg', 'パラメータが不正です。');
 					break;
 
 				// デバイス情報更新
@@ -100,8 +126,23 @@
 					else
 						$SMARTY->assign('inform_msg', 'デバイス情報の更新に成功しました。');
 					break;
+
+				// ベンダー情報更新
+				case 'update_vendor':
+					if (!UtilSQLite::updateVendorInfo(true))
+						$SMARTY->assign('inform_msg', 'ベンダー情報の更新に失敗しました。');
+					else
+						$SMARTY->assign('inform_msg', 'ベンダー情報の更新に成功しました。');
+					break;
 			}
 		}
+
+		// メニュー
+		$param = array();
+		$param[] = UtilString::buildQueryString(array('mode' => 'append_form1'));
+		$param[] = UtilString::buildQueryString(array('mode' => 'update_device'));
+		$param[] = UtilString::buildQueryString(array('mode' => 'update_vendor'));
+		$SMARTY->assign('menu_param', $param);
 
 		// 登録済みリスト作成
 		$res_data1 = array();
@@ -135,7 +176,7 @@
 			$param['mac_addr'] = $entry['mac_addr'];
 			$param['ip_addr'] = $entry['ip_addr'];
 			$tmp_str1 = UtilString::buildQueryString($param);
-			$param['mode'] = 'append_form';
+			$param['mode'] = 'append_form2';
 			$tmp_str2 = UtilString::buildQueryString($param);
 			$res_data2[] = array(
 				'ip_addr'       => $entry['ip_addr'],
@@ -147,16 +188,10 @@
 		}
 
 		// Smarty処理
-		$SMARTY->assign('home_url',     HOME_URL);
-		$SMARTY->assign('res_data1',    $res_data1);
-		$SMARTY->assign('res_data2',    $res_data2);
-		$SMARTY->assign('sess_data',    $SESS_DATA);
-		$SMARTY->assign('upd_ven_param', UtilString::buildQueryString(
-			array('mode' => 'update_vendor', 'mac_addr' => 'dummy')
-		));
-		$SMARTY->assign('upd_dev_param', UtilString::buildQueryString(
-			array('mode' => 'update_device', 'mac_addr' => 'dummy')
-		));
+		$SMARTY->assign('home_url',  HOME_URL);
+		$SMARTY->assign('res_data1', $res_data1);
+		$SMARTY->assign('res_data2', $res_data2);
+		$SMARTY->assign('sess_data', $SESS_DATA);
 		$SMARTY->display('menu.html');
 	}
 	else
